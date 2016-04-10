4361ac2755cebda1632ce494ed65b343e22e0fa5
Eric M Butler <ericbutler1@Erics-MacBook-Pro-2.local>
2016-04-10 11:37:51 -0400

    Prepping for iap config

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+	name: 'Chester-Testing',
+	description: 'Testing android inapp purchases',
+	version: '0.0.1'
+});
+
+App.icons({
+	'android_ldpi': 'resources/icons/icon-36-ldpi.png',
+	'android_mdpi': 'resources/icons/icon-48-mdpi.png',
+	'android_hdpi': 'resources/icons/icon-72-hdpi.png',
+	'android_xhdpi': 'resources/icons/icon-96-xhdpi.png'
+});
