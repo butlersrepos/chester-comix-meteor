0816bd5c399c7018dd51e7c45073cdf6739a722b
Eric Butler <ericbutler000@gmail.com>
2016-04-10 14:31:51 -0400

    Added images to get android publishing enabled. Added mobiel config for billing key

--- mobile-config.js
+++ mobile-config.js
@@ -10,3 +10,7 @@ App.icons({
 	'android_hdpi': 'resources/icons/icon-72-hdpi.png',
 	'android_xhdpi': 'resources/icons/icon-96-xhdpi.png'
 });
+
+App.configurePlugin('cc.fovea.cordova.purchase', {
+	BILLING_KEY : "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArSXSUmR3SzQA8PldOGsyn/+MHOGCqN/jyFjYgPttjT0Gg/bvxyCFerFEXw6QbqGfTh7B9mT05EjGQ6TUbOqPWeU3h7F06HutqI2vHARnwDxLlKT7cB2whzSuBjDoZPZlwI3g1AF5JfJAPEm52un8Xi8AvEtwZa5Nvz81x0WUzDVPB5RGx+NL5YNn1sEllaff09rTsqBUg1z/Nye73HBF3CeeL8KVs6mxqEu37E5itcsDSPdX1G0yFnZg/yxa7nwiAFeJEEQGXjPEcvZlwklGj1wQ9I+RDQUAlizEjU6xhQt6BktBF45zBahMNBCTjk1oDileQVVslmm8gbrvbmt8QQIDAQAB"
+});
